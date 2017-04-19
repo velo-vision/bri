50f1c719f149c84c49a89b3a2f84062caaa68299
jorge <jorge.f@velosoft.mx>
2017-04-19 17:17:09 -0500

    Ajusto blog

--- wp-content/themes/brickcode/searchform.php
+++ wp-content/themes/brickcode/searchform.php
@@ -2,7 +2,7 @@
 	<div id="form1" class="s12 m12 l12">
 		<div class="col s10 m10 l10">
 			<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-				<input type="search" placeholder=" Buscar propiedades" value="<?php the_search_query(); ?>" name="s" id="s"/>
+				<input type="search" placeholder=" Buscar propiedad" value="<?php the_search_query(); ?>" name="s" id="s"/>
 		</div>
 		<div class="s2 m2 l2" style="height: 26px; padding-top: 23px;">
 	 		<input type=image src="<?php bloginfo('template_url'); ?>/images/buscar.png" class="icono">
